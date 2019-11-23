d5cc6b753f9f58051fd0401e999e9db9523e0057
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-11-23 19:33:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,6 @@
 // const Discord = require("discord.js");
 const prefix = "$";
 
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-
-
-
 client.on("message", async message => {
     var command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
